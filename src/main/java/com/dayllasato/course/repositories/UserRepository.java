cb1cdb61cbcdee99e80d8bbdadfeadf8d0ed713e
Daylla Sato <daylla_vilefort@hotmail.com>
2020-08-06 15:37:34 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/dayllasato/course/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.dayllasato.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dayllasato.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+	
+	
+	
+}
