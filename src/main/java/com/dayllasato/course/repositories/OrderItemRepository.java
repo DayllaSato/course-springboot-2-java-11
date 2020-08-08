b0bdc1bd849eea3291c4012b2ede4e4fd4deacb1
Daylla Sato <daylla_vilefort@hotmail.com>
2020-08-08 20:45:42 -0300

    OrderItem, many-to-many association with attributes

---
+++ src/main/java/com/dayllasato/course/repositories/OrderItemRepository.java
@@ -0,0 +1,14 @@
+package com.dayllasato.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dayllasato.course.entities.OrderItem;
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+	
+	
+	
+	
+}
