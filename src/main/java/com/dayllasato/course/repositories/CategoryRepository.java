47bce1363c62895fc01ee2d7259e0bd2579f57e2
Daylla Sato <daylla_vilefort@hotmail.com>
2020-08-08 18:24:40 -0300

    Category

---
+++ src/main/java/com/dayllasato/course/repositories/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.dayllasato.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dayllasato.course.entities.Category;
+
+
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	
+	
+	
+	
+}
