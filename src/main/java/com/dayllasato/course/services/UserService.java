630a0650f970dbf5422b593e2dd684f2b967d7f2
Daylla Sato <daylla_vilefort@hotmail.com>
2020-08-19 10:23:51 -0300

    Exception handling - findById, delete and update

--- src/main/java/com/dayllasato/course/services/UserService.java
+++ src/main/java/com/dayllasato/course/services/UserService.java
@@ -3,50 +3,58 @@ package com.dayllasato.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.dayllasato.course.entities.User;
 import com.dayllasato.course.repositories.UserRepository;
-
+import com.dayllasato.course.services.exceptions.DatabaseException;
+import com.dayllasato.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
-	
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll()
-	{
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	public User findById(Long id)
-	{
+
+	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
-	public User insert(User obj)
-	{
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	public void delete(Long id)
-	{
-		repository.deleteById(id);
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
-	public User update(Long id, User obj)
-	{
+
+	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
-	private void updateData(User entity, User obj) 
-	{
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
